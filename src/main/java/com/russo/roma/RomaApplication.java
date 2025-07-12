7ef62f79b0ca6d9acb7db49f1cea29d9fe43e6fd
russoagustin <pickrusso.agustin@gmail.com>
2025-07-12 11:06:25 -0300

    Primer commit

---
+++ src/main/java/com/russo/roma/RomaApplication.java
@@ -0,0 +1,13 @@
+package com.russo.roma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/russo/roma/RomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.russo.roma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
